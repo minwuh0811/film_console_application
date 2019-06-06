d961195ed015d9233be6a0c50a63e36aa4b8904b
minwuh081@gmail.com <minwuh081@gmail.com>
2019-06-07 01:34:20 +0200

    6 Unit Test for While Object to Load Data from each Table 90% method coverage and 88% lines coverage

--- test/WhileTest.java
+++ test/WhileTest.java
@@ -3,16 +3,69 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class WhileTest {
+    String DBURL="jdbc:mysql://192.168.99.100:3306/filmTest";
+    String DBUser="root";
+    String DBPassword="password";
+    static String path = System.getProperty("user.home")
+            + java.io.File.separator + "IdeaProjects"
+            + java.io.File.separator + "film_konsollapplikation"
+            + java.io.File.separator + "test"
+            + java.io.File.separator + "Resources";
 
     @Test
-    void whileAdmin() {
+    void whileAdminFirstAdminisLoginName() {
+        DataBaseConnection dataBaseConnection=new DataBaseConnection(DBURL,DBUser,DBPassword);
+        TablesSQL tableSQL=new TablesSQL("admin", "film", "ticket");
+        FileName fileName=new FileName("adminTest", "filmTest", "ticketTest");
+        While whileadmin=new While(tableSQL.getAdministratorDBTable(),dataBaseConnection.getStatement(),fileName.getFileNameAdmin(),path,new AdministratorStorage());
+        assertEquals("minwuh081",whileadmin.getAdministratorStorage().getList().get(0).getAdministratorName());
     }
 
     @Test
-    void whileFilm() {
+    void whileAdminSizeEqualsOne() {
+        DataBaseConnection dataBaseConnection=new DataBaseConnection(DBURL,DBUser,DBPassword);
+        TablesSQL tableSQL=new TablesSQL("admin", "film", "ticket");
+        FileName fileName=new FileName("adminTest", "filmTest", "ticketTest");
+        While whileAdmin=new While(tableSQL.getAdministratorDBTable(),dataBaseConnection.getStatement(),fileName.getFileNameAdmin(),path,new AdministratorStorage());
+        assertEquals(1,whileAdmin.getAdministratorStorage().getList().size());
     }
 
     @Test
-    void whileTicket() {
+    void whileFilmFirstNameOfFilm() {
+        DataBaseConnection dataBaseConnection=new DataBaseConnection(DBURL,DBUser,DBPassword);
+        TablesSQL tableSQL=new TablesSQL("admin", "film", "ticket");
+        FileName fileName=new FileName("adminTest", "filmTest", "ticketTest");
+        While whileFilm=new While(tableSQL.getFilmDBTable(),dataBaseConnection.getStatement(),fileName.getFileNameFilm(),path,new FilmStorage());
+        assertEquals("Star War",whileFilm.getFilmStorage().getList().get(0).getName());
+    }
+
+
+    @Test
+    void whileFilmSizeOne() {
+        DataBaseConnection dataBaseConnection=new DataBaseConnection(DBURL,DBUser,DBPassword);
+        TablesSQL tableSQL=new TablesSQL("admin", "film", "ticket");
+        FileName fileName=new FileName("adminTest", "filmTest", "ticketTest");
+        While whileFilm=new While(tableSQL.getFilmDBTable(),dataBaseConnection.getStatement(),fileName.getFileNameFilm(),path,new FilmStorage());
+        assertEquals(1,whileFilm.getFilmStorage().getList().size());
+    }
+
+    @Test
+    void whileTicketSizeOne() {
+        DataBaseConnection dataBaseConnection=new DataBaseConnection(DBURL,DBUser,DBPassword);
+        TablesSQL tableSQL=new TablesSQL("admin", "film", "ticket");
+        FileName fileName=new FileName("adminTest", "filmTest", "ticketTest");
+        While whileFilm=new While(tableSQL.getFilmDBTable(),dataBaseConnection.getStatement(),fileName.getFileNameFilm(),path,new FilmStorage());
+        While whileTicket=new While(tableSQL.getTicketDBTable(),dataBaseConnection.getStatement(),fileName.getFileNameTicket(),path,whileFilm.getFilmStorage(),new TicketStorage());
+        assertEquals(1,whileTicket.getTicketStorage().getList().size());
+    }
+
+    @Test
+    void whileTicketFirstTicketID() {
+        DataBaseConnection dataBaseConnection=new DataBaseConnection(DBURL,DBUser,DBPassword);
+        TablesSQL tableSQL=new TablesSQL("admin", "film", "ticket");
+        FileName fileName=new FileName("adminTest", "filmTest", "ticketTest");
+        While whileFilm=new While(tableSQL.getFilmDBTable(),dataBaseConnection.getStatement(),fileName.getFileNameFilm(),path,new FilmStorage());
+        While whileTicket=new While(tableSQL.getTicketDBTable(),dataBaseConnection.getStatement(),fileName.getFileNameTicket(),path,whileFilm.getFilmStorage(),new TicketStorage());
+        assertEquals("TK1",whileTicket.getTicketStorage().getList().get(0).getTicketID());
     }
 }
