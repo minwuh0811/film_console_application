89aec7d52e014ac0a65d45ba3da8c379f92cbf89
minwuh081@gmail.com <minwuh081@gmail.com>
2019-06-07 00:49:45 +0200

    UnitTest for Data load from each Table

---
+++ test/WhileTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WhileTest {
+
+    @Test
+    void whileAdmin() {
+    }
+
+    @Test
+    void whileFilm() {
+    }
+
+    @Test
+    void whileTicket() {
+    }
+}
