83bd15178211e9c142e2c815ce56126081828053
minwuh081@gmail.com <minwuh081@gmail.com>
2019-06-09 00:42:14 +0200

    Add default string to "AD", "FM" and "TK" fixed Return ID method to return correct ID

--- src/Storage.java
+++ src/Storage.java
@@ -46,15 +46,21 @@ public class Storage {
     public int returnID() {
         int length = IDlist.size();
         int Result = -1;
-        for (int n = 1; n <= length; n++) {
+        for (int n = 1; n <=length; n++) {
             String loop = string + n;
             if (!IDlist.get(n-1).equals(loop)) {
                 Result = n;
-                break;
+                return Result;
             }
         }
-        return Result;
+        return Result=length+1;
     }
 
+    public String getString() {
+        return string;
+    }
 
+    public void setString(String string) {
+        this.string = string;
+    }
 }
