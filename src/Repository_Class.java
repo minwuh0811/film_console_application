688135d810c83494513434f58139bd796afc42dc
minwuh081@gmail.com <minwuh081@gmail.com>
2019-06-08 22:35:47 +0200

    add  savetofile method

--- src/Repository_Class.java
+++ src/Repository_Class.java
@@ -1,12 +1,8 @@
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
 
-public class Repository_Class implements Repository{
-
-    public Repository_Class(Writer fw) {
-        this.fw = fw;
-    }
-
+public class Repository_Class {
     private Writer fw;
 
     public Writer getFw() {
@@ -17,6 +13,14 @@ public class Repository_Class implements Repository{
         this.fw = fw;
     }
 
+    public Repository_Class(){
+
+    }
+
+    public Repository_Class(Writer fw) {
+        this.fw = fw;
+    }
+
     public void write(String text) {
         try {
             fw.write(text);
@@ -24,4 +28,15 @@ public class Repository_Class implements Repository{
             e.printStackTrace();
         }
     }
+
+    public void SaveTofile(java.io.File file, String text) {
+        try (
+                FileWriter filewriter = new FileWriter(file)) {
+            Repository_Class repository = new Repository_Class(filewriter);
+            Main.WriteTextFile(repository, text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
