115e27c637ad57baa092b345bec9c62a24de6ca1
minwuh081@gmail.com <minwuh081@gmail.com>
2019-06-10 17:59:04 +0200

    rewrite the Ticket Object

--- src/Ticket.java
+++ src/Ticket.java
@@ -1,57 +1,56 @@
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Ticket implements Serializable {
-    private static int ID;
-    private String ticketID;
     private Film film;
-    private int price;
     private int numberOfTicket;
-    static int TotalofTicket;
     public static final long serialVersionUID = 3L;
+    private AlterSQLData alterSQLData;
 
-    Ticket(){
+    Ticket() {
 
     }
 
-    public Ticket(Film film, int numberofTicket){
-        this.film=film;
-        this.numberOfTicket=numberofTicket;
-        TotalofTicket-=numberOfTicket;
+    public Ticket(Film film, int numberofTicket) {
+        this.film = film;
+        this.numberOfTicket = numberofTicket;
+        updatedTotalTicket(film,numberOfTicket);
     }
 
-    public Ticket(Film film, int price, int totalofTicket) {
+   /* public Ticket(Film film, int price, int totalofTicket) {
         this.ticketID="TK"+ ID;
         this.film=film;
         this.price=price;
         TotalofTicket=totalofTicket;
     }
 
+
     public Ticket(String ticketID, Film film, int price, int totalofTicket) {
         this.ticketID=ticketID;
         this.film=film;
         this.price=price;
         TotalofTicket=totalofTicket;
+    }*/
+
+    private boolean updatedTotalTicket(Film film, int numberOfTicket) {
+        int updatedTotalTicket = film.getTotalofTicket() - numberOfTicket;
+        if (updatedTotalTicket>=0)  {
+            film.setTotalofTicket(updatedTotalTicket);
+            return true;
+        } else {
+            System.out.println("The Number of the Ticket is not enough.");
+            return false;
+        }
+
+
+
     }
+
 
     public int getNumberOfTicket() {
         return numberOfTicket;
     }
 
-    public static int getID() {
-        return ID;
-    }
-
-    public static void setID(int ID) {
-        Ticket.ID = ID;
-    }
-
-    public String getTicketID() {
-        return ticketID;
-    }
-
-    public void setTicketID(String ticketID) {
-        this.ticketID = ticketID;
-    }
 
     public Film getFilm() {
         return film;
@@ -61,12 +60,8 @@ public class Ticket implements Serializable {
         this.film = film;
     }
 
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
+    public void setNumberOfTicket(int numberOfTicket) {
+        this.numberOfTicket = numberOfTicket;
     }
 
 }
